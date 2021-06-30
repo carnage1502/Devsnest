a19e244951499f7be709b302795d59b66c2c8422
SHIVAM SHARMA <75442551+carnage1502@users.noreply.github.com>
2021-06-30 11:54:12 +0530

    Event-Handler
    Devsnest Frontend THA -14

---
+++ Frontend/Day14/logic.js
@@ -0,0 +1,17 @@
+function onClick() {
+    console.log("I am Clicked");
+  }
+  
+  function ondoubClick() {
+    console.log("I am Double Clicked");
+  }
+  
+  function keyDown() {
+    console.log("Key was down");
+  }
+  function keyUp() {
+    console.log("Key was up");
+  }
+  function mMove() {
+    console.log("Mouse Moved");
+  }
