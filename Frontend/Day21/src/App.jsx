3202e9305781192e65fe797efd1cbaa1b3d4d9e7
SHIVAM SHARMA <75442551+carnage1502@users.noreply.github.com>
2021-07-18 22:15:45 +0530

    Add files via upload
    Tha-21 Devsnet

---
+++ Frontend/Day21/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import AddItemForm from "./components/Card";
+import RenderItem from "./components/comp";
+import './style.css';
+
+const App = () => {
+  const [items, setItems] = useState([]);
+
+  const updateItem = (index, newItem) => {
+    setItems(
+      items.map((item, i) => (i === index ? { ...item, ...newItem } : item))
+    );
+  };
+
+  const addItem = (item) => {
+    setItems([...items, item]);
+  };
+
+  const removeItem = (index) => {
+    setItems(items.filter((item, i) => i !== index));
+  };
+
+  return (
+    <div className="container d-flex flex-column align-items-center">
+      <AddItemForm addItem={addItem} />
+      {items.length === 0 ? (
+        <h3 className="text-center mt-4">
+          Start Adding Items
+        </h3>
+      ) : (
+        items.map((item, index) => (
+          <RenderItem
+            item={item}
+            key={index}
+            index={index}
+            updateItem={updateItem}
+            removeItem={removeItem}
+          />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default App;
