a2f7a94e7d876cd98e88c9d4aeb02ce7936058d2
SHIVAM SHARMA <75442551+carnage1502@users.noreply.github.com>
2021-09-30 20:44:29 +0530

    Connecting Redis and postgres with express

---
+++ Backend/day12/controllers/register.js
@@ -0,0 +1,29 @@
+const User = require("../models/mongo");
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
+const register = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const alreadyExists = await User.findOne({ where: { email } }).exec();
+    if (alreadyExists) {
+      res.status(401).send("Email already exists");
+    }
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hash = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      email: email.toLowerCase(),
+      password: hash,
+      fullName: "Ansh",
+    });
+    const savedUser = await newUser.save();
+    res.status(201).send(savedUser);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Something went wrong");
+  }
+};
+
+module.exports = register;
